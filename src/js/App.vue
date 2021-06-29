ada9902937b1f48fd9a23b2534b00538ec0db28b
Your Name <you@example.com>
2021-06-29 16:57:16 -0400

    Estrutura-VUE

---
+++ src/js/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Teste from './Teste.vue';
+
+export default {
+  data() {
+    return {
+      msg:'Olá Mundo!'
+    }
+  },
+  components:{
+    Teste
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+    <teste />
+  </div>
+</template>
+
+<style>
+h1 {
+  background-color: #FF0000;
+  color: #FFFFFF;
+}
+</style>
---
+++ src/js/Teste.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>{{t}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        t:'Carlos'
+      }
+    }
+  }
+</script>
+
+<style>
+span {
+  color: #FF0000;
+}
+</style>
